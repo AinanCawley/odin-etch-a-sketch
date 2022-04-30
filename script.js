ca35466bd4d61556fb86585ddec3258f4a5bce4e
AinanCawley <ainan.c.cawley@gmail.com>
2022-04-30 13:54:16 +0800

    Add eventListener to cells

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ const cellsCreator = document.createDocumentFragment();
 for( let i = 0; i < 16; i++ )
 {
     row = document.createElement('div');
-    row.setAttribute('id', i+"");
     row.setAttribute('class', "row");
-    
+
     for( let j = 0; j < 16; j++ )
     {
         cell = document.createElement('div');
-        cell.setAttribute('id', j+"");
+        cell.setAttribute('id', i+"x"+j);
         cell.setAttribute('class', 'cell');
         row.appendChild(cell);
     }
@@ -19,3 +18,14 @@ for( let i = 0; i < 16; i++ )
 }
 
 etchSketch.appendChild(cellsCreator);
+
+const cells = document.getElementsByClassName('cell');
+const numberOfCells = cells.length;
+
+for( let i = 0; i < numberOfCells; i++ )
+{
+    cells[i].addEventListener('mouseover', function(event)
+    {
+        console.log(event);
+    });
+}
