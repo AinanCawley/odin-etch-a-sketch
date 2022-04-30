6916536954f9014888c7121f74fec60d24cd747b
AinanCawley <ainan.c.cawley@gmail.com>
2022-04-30 13:30:42 +0800

    Make cellsCreator create a grid of cells with 16 rows and 16 cells per row

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const cellsCreator = document.createDocumentFragment();
 
 for( let i = 0; i < 16; i++ )
 {
+    row = document.createElement('div');
+    row.setAttribute('id', i+"");
+    row.setAttribute('class', "row");
+    
     for( let j = 0; j < 16; j++ )
     {
         cell = document.createElement('div');
-        cell.setAttribute('id', i+"x"+j );
+        cell.setAttribute('id', j+"");
         cell.setAttribute('class', 'cell');
-        cellsCreator.appendChild(cell);
+        row.appendChild(cell);
     }
+
+    cellsCreator.appendChild(row);
 }
 
 etchSketch.appendChild(cellsCreator);
