25627d00b0dbe807fbc24f51d4798b20428f656f
AinanCawley <ainan.c.cawley@gmail.com>
2022-05-01 11:09:24 +0800

    Add grid dimension info and button to change dimensions

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const etchSketch = document.getElementById('etchSketch');
 const cellsCreator = document.createDocumentFragment();
 
-for( let i = 0; i < 16; i++ )
+let numberOfRows = 16;
+let numberOfColumns = 16;
+
+const sizeInfo = document.createElement('p');
+sizeInfo.innerText = numberOfRows + " rows by " + numberOfColumns + " columns";
+const interactive = document.getElementById('interactive');
+interactive.appendChild(sizeInfo);
+
+const sizeButton = document.createElement('button');
+sizeButton.innerText = "change grid dimensions";
+sizeButton.addEventListener('click', function(event)
+{
+    console.log(event);
+});
+interactive.appendChild(sizeButton);
+
+
+for( let i = 0; i < numberOfRows; i++ )
 {
     row = document.createElement('div');
     row.setAttribute('class', "row");
 
-    for( let j = 0; j < 16; j++ )
+    for( let j = 0; j < numberOfColumns; j++ )
     {
         const cell = document.createElement('div');
         cell.setAttribute('id', i+"x"+j);
@@ -26,7 +43,6 @@ for( let i = 0; i < numberOfCells; i++ )
 {
     cells[i].addEventListener('mouseover', function(event)
     {
-
         event.target.style.backgroundColor = "hsl( " + Math.floor(Math.random() * 360) + ", 100%, 50%)";
     });
 }
